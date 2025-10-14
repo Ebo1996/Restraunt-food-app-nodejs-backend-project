575e925d66ed6978646b7a994ea4d7131f2b6895
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-14 21:01:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Welcome to the server</h1>');
 });
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
