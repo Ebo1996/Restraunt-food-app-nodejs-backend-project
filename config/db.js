f54faf9dc02067dec3cabe83da4aef4e0e7696be
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-19 10:24:19 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ const colors = require("colors");
 //function mongodb database connection
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGODB_URL); // Fixed variable name
     console.log(`Connected To Database ${mongoose.connection.host}`.bgCyan);
   } catch (error) {
     console.log("DB Error", error);
   }
 };
 
-module.exports = connectDb;
+module.exports = connectDb; // Exporting as connectDb
