71736f586040a2ae9116af05ea4ec6ac757017b0
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-20 22:38:29 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,36 @@
+const userModel = require("../models/userModel");
+
 // REGISTER
 const registerController = async (req, res) => {
   try {
+    const { username, email, password, phone, address } = req.body;
+
+    // validation
+    if (!username || !email || !password || !address || !phone) {
+      return res.status(500).send({
+        success: false,
+        message: 'Please Provide All Fields'
+      });
+    }
+
+    // check user
+    const existing = await userModel.findOne({ email });
+    if (existing) {
+      return res.status(500).send({
+        success: false,
+        message: 'Email Already Registered please Login'
+      });
+    }
+
+    //create new user
+    const user = await userModel.create({ username, email, password, address, phone })
+    res.status(201).send({
+      success: true,
+      message: 'succesfully Registered'
+    });
+
+    // Add user creation logic here
+    // const newUser = await userModel.create({...});
 
   } catch (error) {
     console.log(error);
@@ -10,6 +40,6 @@ const registerController = async (req, res) => {
       error
     });
   }
-}; 
+};
 
-module.exports = { registerController };
+module.exports = registerController;
