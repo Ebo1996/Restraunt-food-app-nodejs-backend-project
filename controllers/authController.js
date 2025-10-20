5916c1bc07aa59dd2cc35ff21da55d9a242c2439
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-20 22:04:59 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,15 @@
+// REGISTER
 const registerController = async (req, res) => {
   try {
-    
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: 'Error In Register API',
+      error
+    });
   }
-  catch (error) { 
-    console.log(object)
-  }
-
-
-
-
-
-
-
-
-
-
-
-
 };
+
+module.exports = { registerController };
