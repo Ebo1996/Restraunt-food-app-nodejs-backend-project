1a74ffcbd20310ca07abf45a489cc87ef32e1a32
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-22 21:10:32 +0300

    Update testController.js

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,8 +1,18 @@
-const testUserController = () => { 
+// testController.js - Fix the incomplete function
+const testUserController = (req, res) => {
   try {
-   
-}
-
+    res.status(200).send({
+      success: true,
+      message: "Test User API"
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Test API",
+      error
+    });
+  }
 };
 
 module.exports = { testUserController };
