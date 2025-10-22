2b304a8614be603460723d25fe5d8de53bcc9179
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-22 21:10:39 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,9 @@
+// authRoutes.js - Fix the import statement
 const express = require('express')
 const router = express.Router()
+const { registerController } = require('../controllers/authController')
 
-//REGISTER || POST
+// REGISTER || POST
 router.post('/register', registerController)
 
 module.exports = router
