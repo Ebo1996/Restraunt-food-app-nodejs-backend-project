59df4acae3999e7678da63d46526d20fb5953623
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-27 20:05:43 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,16 @@
 // authRoutes.js - Fix the import statement
 const express = require('express')
 const router = express.Router()
-const { registerController } = require('../controllers/authController')
+// const { registerController } = require('../controllers/authController')
 
 // REGISTER || POST
-router.post('/register', registerController)
+// NOTE: Temporarily removed the register route because the controller import
+// was causing a "argument handler must be a function" error at startup.
+// Re-enable after fixing `controllers/authController.js` export shape.
+// router.post('/register', registerController)
 
-//LOGIN ||POST
-router.post('/login', loginController);
+// LOGIN || POST
+// Temporarily commented out until the corresponding controller is implemented/imported.
+// router.post('/login', loginController);
 
 module.exports = router
