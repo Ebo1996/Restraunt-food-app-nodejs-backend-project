9d85a8f8a655a72c1045d1028e95f375b11681b3
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-27 21:23:01 +0300

    Create securityMiddleware.js

---
+++ middleware/securityMiddleware.js
@@ -0,0 +1,28 @@
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const securityMiddleware = [
+  helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" }
+  })
+];
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Too many requests from this IP, please try again later',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: 'Too many login attempts, please try again later',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+module.exports = { securityMiddleware, limiter, authLimiter };
+
+
